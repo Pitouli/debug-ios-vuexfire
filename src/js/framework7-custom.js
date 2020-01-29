569a9dcd6c9311e4ad1d085d053a18c45d2b9f9d
Pitouli <pitouli@gmail.com>
2020-01-29 21:57:32 +0100

    Framework7 Base App

---
+++ src/js/framework7-custom.js
@@ -0,0 +1,79 @@
+
+import Framework7 from 'framework7/framework7-lite.esm.js';
+import Appbar from 'framework7/components/appbar/appbar.js';
+import Dialog from 'framework7/components/dialog/dialog.js';
+import Popup from 'framework7/components/popup/popup.js';
+import LoginScreen from 'framework7/components/login-screen/login-screen.js';
+import Preloader from 'framework7/components/preloader/preloader.js';
+import Progressbar from 'framework7/components/progressbar/progressbar.js';
+import Swipeout from 'framework7/components/swipeout/swipeout.js';
+import Accordion from 'framework7/components/accordion/accordion.js';
+import ContactsList from 'framework7/components/contacts-list/contacts-list.js';
+import VirtualList from 'framework7/components/virtual-list/virtual-list.js';
+import ListIndex from 'framework7/components/list-index/list-index.js';
+import Timeline from 'framework7/components/timeline/timeline.js';
+import Tabs from 'framework7/components/tabs/tabs.js';
+import Panel from 'framework7/components/panel/panel.js';
+import Card from 'framework7/components/card/card.js';
+import Chip from 'framework7/components/chip/chip.js';
+import Form from 'framework7/components/form/form.js';
+import Input from 'framework7/components/input/input.js';
+import Checkbox from 'framework7/components/checkbox/checkbox.js';
+import Radio from 'framework7/components/radio/radio.js';
+import Toggle from 'framework7/components/toggle/toggle.js';
+import SmartSelect from 'framework7/components/smart-select/smart-select.js';
+import Grid from 'framework7/components/grid/grid.js';
+import InfiniteScroll from 'framework7/components/infinite-scroll/infinite-scroll.js';
+import PullToRefresh from 'framework7/components/pull-to-refresh/pull-to-refresh.js';
+import Lazy from 'framework7/components/lazy/lazy.js';
+import Fab from 'framework7/components/fab/fab.js';
+import Searchbar from 'framework7/components/searchbar/searchbar.js';
+import Swiper from 'framework7/components/swiper/swiper.js';
+import Notification from 'framework7/components/notification/notification.js';
+import Autocomplete from 'framework7/components/autocomplete/autocomplete.js';
+import Tooltip from 'framework7/components/tooltip/tooltip.js';
+import Skeleton from 'framework7/components/skeleton/skeleton.js';
+import Menu from 'framework7/components/menu/menu.js';
+import TextEditor from 'framework7/components/text-editor/text-editor.js';
+import Popover from 'framework7/components/popover/popover.js';
+
+Framework7.use([
+  Appbar,
+  Dialog,
+  Popup,
+  LoginScreen,
+  Preloader,
+  Progressbar,
+  Swipeout,
+  Accordion,
+  ContactsList,
+  VirtualList,
+  ListIndex,
+  Timeline,
+  Tabs,
+  Panel,
+  Card,
+  Chip,
+  Form,
+  Input,
+  Checkbox,
+  Radio,
+  Toggle,
+  SmartSelect,
+  Grid,
+  InfiniteScroll,
+  PullToRefresh,
+  Lazy,
+  Fab,
+  Searchbar,
+  Swiper,
+  Notification,
+  Autocomplete,
+  Tooltip,
+  Skeleton,
+  Menu,
+  TextEditor,
+  Popover
+]);
+
+export default Framework7;
